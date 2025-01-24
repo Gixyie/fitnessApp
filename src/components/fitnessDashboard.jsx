6e6b7ce46ff1f47ae41aed28dcaa73ac97328eb2
Gixyie <giorgiacoco3@hotmail.com>
2025-01-24 17:47:45 +0100

    new app

---
+++ src/components/fitnessDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./fitnessDashboard.css";
+
+const fitnessDashboard = () => {
+  return (
+    <div>fitnessDashboard</div>
+  )
+}
+
+export default fitnessDashboard
